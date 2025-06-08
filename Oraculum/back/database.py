c404ea3faaee6bce54602cb92a1fa8e24a8e50e4
samuelgrzr <samuelgarciazorrilla@gmail.com>
2025-06-08 13:35:19 +0200

    Añadida la url de la base de datos desplegada

--- Oraculum/back/database.py
+++ Oraculum/back/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:12345678@localhost:3306/oraculum"
+# URL_DATABASE = "mysql+pymysql://root:12345678@localhost:3306/oraculum"
+URL_DATABASE = "mysql+pymysql://root:juoQsmsPUdKUuJgWyuCtQvIzKahQHiDm@shortline.proxy.rlwy.net:17383/railway"
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
