24461fe8e4233bcf49b0848065e3936f076f147e
samuelgrzr <samuelgarciazorrilla@gmail.com>
2025-06-08 16:35:54 +0200

    Añadida confirmación del navegador en jwtInterceptor

--- Oraculum/front/src/app/interceptors/jwt.interceptor.ts
+++ Oraculum/front/src/app/interceptors/jwt.interceptor.ts
@@ -1,17 +1,24 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const authService = inject(AuthService);
-  const token = authService.getToken();
+  const platformId = inject(PLATFORM_ID);
   
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+  if (isPlatformBrowser(platformId)) {
+    const authService = inject(AuthService);
+    const token = authService.getToken();
+    
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
   }
+  
   return next(req);
 };
